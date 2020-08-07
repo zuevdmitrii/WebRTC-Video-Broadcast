a539d0f56109583539cf6905514abbfef23dc631
Zuev Dmitriy <42337033+zuevdmitrii@users.noreply.github.com>
2020-08-07 10:05:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 let broadcaster;
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const http = require("http");
 const server = http.createServer(app);
